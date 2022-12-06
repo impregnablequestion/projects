64569a214057ec43c85efb2f5e5166ebf7880a1d
Séamus <ryans40@tcd.ie>
2022-12-06 14:38:22 +0000

    happy functions working in repository

--- project_1/belljar_rota/repositories/staff_repository.py
+++ project_1/belljar_rota/repositories/staff_repository.py
@@ -2,15 +2,15 @@ from db.run_sql import run_sql
 from models.staff_member import Staffmember
 
 def save(staff_member):
-    sql = "INSERT INTO staff (name, min_hours, max_hours) VALUES (%s, %s, %s) RETURNING *"
-    values = [staff_member.name, staff_member.min_hours, staff_member.max_hours]
+    sql = "INSERT INTO staff (name, min_hours, max_hours, happy) VALUES (%s, %s, %s, %s) RETURNING *"
+    values = [staff_member.name, staff_member.min_hours, staff_member.max_hours, staff_member.happy]
     results = run_sql(sql, values)
     staff_member.id = results[0]['id']
     return staff_member
 
 def update(staff_member):
-    sql = "UPDATE staff SET (name, min_hours, max_hours) = (%s, %s, %s) WHERE id = %s RETURNING *"
-    values = [staff_member.name, staff_member.min_hours, staff_member.max_hours, staff_member.id]
+    sql = "UPDATE staff SET (name, min_hours, max_hours) = (%s, %s, %s, %s) WHERE id = %s RETURNING *"
+    values = [staff_member.name, staff_member.min_hours, staff_member.max_hours, staff_member.happy, staff_member.id]
     run_sql(sql, values)
     return staff_member
 
@@ -22,7 +22,7 @@ def select(id):
     values = [id]
     results = run_sql(sql, values)
     result = results[0]
-    staff_member = Staffmember(result["name"], result["min_hours"], result["max_hours"], result["id"])
+    staff_member = Staffmember(result["name"], result["min_hours"], result["max_hours"], result['happy'], result["id"])
     return staff_member
 
 def select_all():
@@ -31,7 +31,7 @@ def select_all():
     sql = "SELECT * FROM staff"
     results = run_sql(sql)
     for row in results:
-        staff_member = Staffmember(row["name"], row["min_hours"], row["max_hours"], row["id"])
+        staff_member = Staffmember(row["name"], row["min_hours"], row["max_hours"], row['happy'], row["id"])
         staff.append(staff_member) 
     return staff
 
